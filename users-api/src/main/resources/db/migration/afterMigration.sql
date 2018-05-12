5121f0fe1498b106b7660d3636df2324d70550a9
kimn5867 <kimn5867@gmail.com>
2018-05-12 13:01:46 -0700

    Initial Version

---
+++ users-api/src/main/resources/db/migration/afterMigration.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS
+	(USER_NAME, FIRST_NAME, LAST_NAME)
+VALUES
+	('user from seeds', 'assfsg', 'asrwega'),
+	('another user', 'jrnrh', 'ahrwa'),
+	('someone', 'eagaga', 'arabr'),
+	('someone else', 'arae', 'sagra');
